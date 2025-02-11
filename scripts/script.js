8d2d450098efffc9dc9bc0661a8492cc675c799d
Ari Osmun <osmunaj@gmail.com>
2025-02-11 10:17:56 -0700

    Test 1 if sidebar script works through cloudflare

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,2 @@
 
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
 
-const str = window.location.href;
-const spl = str.split('/');
-const current = spl[spl.length-1]
-
-
-const links = document.querySelectorAll('.link');
-
-links.forEach((link) => {
-
-    link.classList.remove('selected');
-    let link_str = link.href;
-    const spl2 = link_str.split('/');
-    link_str = spl2[spl2.length-1];
-    if(link_str == current){
-        link.classList.add('selected');
-    }
-});
