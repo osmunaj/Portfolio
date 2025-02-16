7bba6138775a6642ed85fa0359431ee6c9e5cff6
Ari Osmun <osmunaj@gmail.com>
2025-02-16 12:05:33 -0700

    Remove Testing parameters

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,7 @@
 
-
-alert("works!")
-
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-
-const str = window.location.href;
-const spl = str.split('/');
-const current = spl[spl.length-1]
-
-
-const links = document.querySelectorAll('.link');
-
-links.forEach((link) => {
-
-    link.classList.remove('selected');
-    let link_str = link.href;
-    const spl2 = link_str.split('/');
-    link_str = spl2[spl2.length-1];
-    if(link_str == current){
-        link.classList.add('selected');
-    }
-});
+function update_link(element){
+    
+    const temp = document.getElementById(element);
+    temp.classList.add('selected');
+}
+ 
