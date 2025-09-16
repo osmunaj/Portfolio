487de700df3b9e843104fc53eb5b38482493f864
Ari Osmun <osmunaj@gmail.com>
2025-09-16 12:59:24 -0600

    Added more images to About me, added arrows to indicate dropdown menus

--- scripts/collapsible.js
+++ scripts/collapsible.js
@@ -1,8 +1,12 @@
 var coll = document.getElementsByClassName("collapsible");
 
+
+
 for (var i = 0; i < coll.length; i++) {
    
     var content = coll[i].nextElementSibling;
+    const arrow = coll[i].querySelector('.icon_left')
+
     if(content == null){continue;} 
     const cl = content.classList;
     const has_class = cl.contains("open") && cl.contains("close");
@@ -14,10 +18,13 @@ for (var i = 0; i < coll.length; i++) {
         if (isopen) {
             cl.add("close");
             cl.remove("open");
+            arrow.classList.remove("flipped");
+            
         } else {
             cl.add("open");
             cl.remove("close");
-            cl.remove("loaded")
+            cl.remove("loaded");
+            arrow.classList.add("flipped");
         }
     });
 }
